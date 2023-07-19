fa94d1ba9e53a5dab05bc4d93c47fdb7824aa18a
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-07-19 15:30:33 +0200

    Update path_default_file.test.ts

---
+++ src/files/path_default_file.test.ts
@@ -0,0 +1,26 @@
+import { assertEquals } from "https://deno.land/std@0.194.0/testing/asserts.ts";
+import { pathDefaultFile } from "./path_default_file.ts";
+
+Deno.test(
+    'Generate a path with a default file if the path has no file extension.',
+    async (test) => {
+        await test.step({
+            name: 'File extension already exists',
+            fn: () => {
+                assertEquals(pathDefaultFile('path/to/file.txt', 'index.html'), 'path/to/file.txt')
+            }
+        });
+        await test.step({
+            name: 'No file extension exists',
+            fn: () => {
+                assertEquals(pathDefaultFile('path/to/file', 'index.html'), 'path/to/file/index.html')
+            }
+        });
+        await test.step({
+            name: 'Trailing Slash (/)',
+            fn: () => {
+                assertEquals(pathDefaultFile('path/to/file/', 'index.html'), 'path/to/file//index.html')
+            }
+        });
+    }
+)
