ddbd669c22b0859f302c9a62f40c421f146efce0
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-07-19 16:36:41 +0200

    Update get_mime_type.ts

--- src/mime_types/get_mime_type.ts
+++ src/mime_types/get_mime_type.ts
@@ -1,6 +1,6 @@
 import MIMETypesFile from './mime_types.json' assert { type: 'json' }
 
 export function getMIMEType(extension : string) : string {
-    const MIMETypes : {[key : string] : string} = MIMETypesFile
-    return MIMETypes[extension]
+    const MIMETypes : {[key : string] : string} = MIMETypesFile.types
+    return MIMETypes[extension] || MIMETypesFile.default
 }
