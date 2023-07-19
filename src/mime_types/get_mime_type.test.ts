a0b31adcb00d865c3c28720ded755e791d150997
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-07-19 15:21:15 +0200

    Update get_mime_type.test.ts

---
+++ src/mime_types/get_mime_type.test.ts
@@ -0,0 +1,32 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { getMIMEType } from "./get_mime_type.ts";
+
+Deno.test(
+    'Get MIME type for specific file extension',
+    async (test) => {
+        await test.step({
+            name: 'html',
+            fn: () => {
+                assertEquals(getMIMEType('html'), 'text/html');
+            }
+        })
+        await test.step({
+            name: 'css',
+            fn: () => {
+                assertEquals(getMIMEType('css'), 'text/css');
+            }
+        })
+        await test.step({
+            name: 'js',
+            fn: () => {
+                assertEquals(getMIMEType('js'), 'application/javascript');
+            }
+        })
+        await test.step({
+            name: 'txt',
+            fn: () => {
+                assertEquals(getMIMEType('txt'), 'text/plain');
+            }
+        })
+    }
+)
