19a6a70d584021624175df8bc4c01ce080720923
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-07-20 15:05:45 +0200

    Update file_server.ts

--- src/file_server.ts
+++ src/file_server.ts
@@ -1,16 +1,9 @@
 
-import { getFileExtension } from "./files/get_file_extension.ts";
-import { getMIMEType } from "./mime_types/get_mime_type.ts";
-import { pathDefaultFile } from "./files/path_default_file.ts";
+import { FileServerOptions } from "./file_server_options.d.ts";
+import { requestHandler } from "./request/request_handler.ts";
 
 export default function fileServer(
-    options? : {
-        serve? : Deno.ServeOptions | Deno.ServeTlsOptions,
-        directory? : {
-            rootDir? : string,
-            defaultFileName? : string
-        }
-    },
+    options? : FileServerOptions,
 ) {
     Deno.serve(
         {
@@ -19,35 +12,9 @@ export default function fileServer(
                 port
             }) => console.log(`Files serving for ${options?.directory?.rootDir || '.'} on http://${hostname}:${port}/`),
             ...(options?.serve || {}),
-        }, (request) => {
-        
-        const requestURLPath = new URL(request.url).pathname;
-
-        try {
-            const path = pathDefaultFile(
-                `${options?.directory?.rootDir || './html'}${requestURLPath}`,
-                options?.directory?.defaultFileName || 'index.html'
-            )
-            
-            return new Response(
-                Deno.readFileSync(path),
-                {
-                    headers: {
-                        'Content-Type': getMIMEType(getFileExtension(requestURLPath))
-                    }
-                }
-            );
-        } catch (error) {
-            if (error instanceof Deno.errors.NotFound) {
-                return new Response(
-                    '404: Not found!',
-                    {
-                        status: 404
-                    }
-                )
-            } else {
-                return new Response(error);
-            }
-        }
-    })
+        }, (request) => requestHandler(
+            request,
+            options
+        )
+    )
 }
