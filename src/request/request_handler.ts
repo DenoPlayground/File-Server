f5a467c74e0a8d6eb30897a3520acaf5ca0882ec
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-07-20 15:05:49 +0200

    Create request_handler.ts

---
+++ src/request/request_handler.ts
@@ -0,0 +1,36 @@
+import { FileServerOptions } from "../file_server_options.d.ts";
+import { getFileExtension } from "../files/get_file_extension.ts";
+import { pathDefaultFile } from "../files/path_default_file.ts";
+import { getMIMEType } from "../mime_types/get_mime_type.ts";
+
+export function requestHandler(request : Request, options : FileServerOptions) {
+        
+    const requestURLPath = new URL(request.url).pathname;
+
+    try {
+        const path = pathDefaultFile(
+            `${options?.directory?.rootDir || './html'}${requestURLPath}`,
+            options?.directory?.defaultFileName || 'index.html'
+        )
+        
+        return new Response(
+            Deno.readFileSync(path),
+            {
+                headers: {
+                    'Content-Type': getMIMEType(getFileExtension(requestURLPath))
+                }
+            }
+        );
+    } catch (error) {
+        if (error instanceof Deno.errors.NotFound) {
+            return new Response(
+                '404: Not found!',
+                {
+                    status: 404
+                }
+            )
+        } else {
+            return new Response(error);
+        }
+    }
+}
