85b3a95c30e58014ac63b1c0f4c1f2cb2edab9ca
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-10-17 16:22:20 +0200

    Update path_default_file.test.ts

--- src/files/path_default_file.test.ts
+++ src/files/path_default_file.test.ts
@@ -7,19 +7,31 @@ Deno.test(
         await test.step({
             name: 'File extension already exists',
             fn: () => {
-                assertEquals(pathDefaultFile('path/to/file.txt', 'index.html'), 'path/to/file.txt')
+                assertEquals(pathDefaultFile('path/to/file.txt', 'index.html'), 'path/to/file.txt');
             }
         });
         await test.step({
             name: 'No file extension exists',
             fn: () => {
-                assertEquals(pathDefaultFile('path/to/file', 'index.html'), 'path/to/file/index.html')
+                assertEquals(pathDefaultFile('path/to/file', 'index.html'), 'path/to/file/index.html');
             }
         });
         await test.step({
             name: 'Trailing Slash (/)',
             fn: () => {
-                assertEquals(pathDefaultFile('path/to/file/', 'index.html'), 'path/to/file//index.html')
+                assertEquals(pathDefaultFile('path/to/file/', 'index.html'), 'path/to/file/index.html');
+            }
+        });
+        await test.step({
+            name: 'No path exists',
+            fn: () => {
+                assertEquals(pathDefaultFile('', 'index.html'), 'index.html');
+            }
+        });
+        await test.step({
+            name: 'Path with special characters',
+            fn: () => {
+                assertEquals(pathDefaultFile('pa.th/t.o/f[il)e/', 'index.html'), 'pa.th/t.o/f[il)e/index.html');
             }
         });
     }
