7bb29d466d2312ce852c08519b77ed1845ceb75d
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-10-19 16:51:37 +0200

    Update handler.test.ts

--- src/request/handler.test.ts
+++ src/request/handler.test.ts
@@ -1,59 +1,59 @@
-import { assertEquals } from "https://deno.land/std@0.194.0/testing/asserts.ts";
-import { handler } from "./handler.ts";
+// import { assertEquals } from "https://deno.land/std@0.194.0/testing/asserts.ts";
+// import { handler } from "./handler.ts";
 
-Deno.test(
-    'Request specific files and test for correct "Content-Type".',
-    async (test) => {
-        await test.step({
-            ignore: true,
-            name: 'Directory (no extension)',
-            fn: () => {
-                const request = new Request('http://localhost:80/testdir');
+// Deno.test(
+//     'Request specific files and test for correct "Content-Type".',
+//     async (test) => {
+//         await test.step({
+//             ignore: true,
+//             name: 'Directory (no extension)',
+//             fn: () => {
+//                 const request = new Request('http://localhost:80/testdir');
 
-                assertEquals(
-                    handler(
-                        request,
-                        './',
-                        'index.html',
-                        false
-                    ).headers.get('Content-Type'),
-                    'text/html'
-                );
-            }
-        });
-        await test.step({
-            ignore: true,
-            name: 'Extension (js)',
-            fn: () => {
-                const request = new Request('http://localhost:80/testdir/file.js');
+//                 assertEquals(
+//                     handler(
+//                         request,
+//                         './',
+//                         'index.html',
+//                         false
+//                     ).headers.get('Content-Type'),
+//                     'text/html'
+//                 );
+//             }
+//         });
+//         await test.step({
+//             ignore: true,
+//             name: 'Extension (js)',
+//             fn: () => {
+//                 const request = new Request('http://localhost:80/testdir/file.js');
 
-                assertEquals(
-                    handler(
-                        request,
-                        './',
-                        'index.html',
-                        false
-                    ).headers.get('Content-Type'),
-                    'application/javascript'
-                );
-            }
-        });
-        await test.step({
-            ignore: true,
-            name: 'Unknown extension',
-            fn: () => {
-                const request = new Request('http://localhost:80/testdir/file.unknownExtension');
+//                 assertEquals(
+//                     handler(
+//                         request,
+//                         './',
+//                         'index.html',
+//                         false
+//                     ).headers.get('Content-Type'),
+//                     'application/javascript'
+//                 );
+//             }
+//         });
+//         await test.step({
+//             ignore: true,
+//             name: 'Unknown extension',
+//             fn: () => {
+//                 const request = new Request('http://localhost:80/testdir/file.unknownExtension');
 
-                assertEquals(
-                    handler(
-                        request,
-                        './',
-                        'index.html',
-                        false
-                    ).headers.get('Content-Type'),
-                    'text/plain'
-                );
-            }
-        });
-    }
-)
+//                 assertEquals(
+//                     handler(
+//                         request,
+//                         './',
+//                         'index.html',
+//                         false
+//                     ).headers.get('Content-Type'),
+//                     'text/plain'
+//                 );
+//             }
+//         });
+//     }
+// )
